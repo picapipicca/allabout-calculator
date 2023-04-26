2de84cd6f95b9dad02d98da46a120321f72a7f86
picapipicca <haewon09106@gmail.com>
2023-04-27 02:07:29 +0900

    modify:firebase 관련 삭제

--- next.config.js
+++ next.config.js
@@ -1,17 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // webpack: (config, { isServer }) => {
-  //   if (!isServer) {
-  //     config.resolve.fallback = {
-  //       fs: false,
-  //       tls:false,
-  //       net:false,
-  //     };
-  //   }
 
-  //   return config;
-  // },
 };
 
 module.exports = nextConfig;
