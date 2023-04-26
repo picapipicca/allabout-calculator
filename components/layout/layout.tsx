35f156d8e92241fd79c9b49aa34a867d19e6858d
picapipicca <haewon09106@gmail.com>
2023-04-27 02:12:07 +0900

    modify:mainheader delete

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -1,11 +1,9 @@
-import MainHeader from "./mainHeader";
 import Nav from "./Nav";
 
 const Layout = (props: any) => {
   return (
     <>
-      {/* <MainHeader /> */}
-      <Nav/>
+      <Nav />
       <main>{props.children}</main>
     </>
   );
