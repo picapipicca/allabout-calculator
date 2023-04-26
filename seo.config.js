434965161e6c31bde708b737e5371ac2bee5185f
picapipicca <haewon09106@gmail.com>
2023-04-26 23:36:34 +0900

    new:robots.txt next-seo

---
+++ seo.config.js
@@ -0,0 +1,30 @@
+export default {
+  titleTemplate: "%s - 올 어바웃 계산기",
+  additionalLinkTags: [
+    // {
+    //   rel: 'icon',
+    //   href: '/favicon.ico',
+    // },
+    // {
+    //   rel: 'manifest',
+    //   href: '/manifest.json',
+    // },
+  ],
+  openGraph: {
+    type: "website",
+    locale: "ko_KR",
+    url: "https://allcalculator.shop",
+    site_name: "올 어바웃 계산기",
+    title: "2023년 연봉 실수령액 계산기, bmi 계산기, 글자수 세기 , 다른 글자 찾기",
+    description:
+      "2023년 내 연봉에서 통장에 들어오는 실수령액부터 내 체지방, 자소서 쓸 떄 편리한 글자수 세기와 텍스트 다른곳 찾기 기능까지 한곳에서 알아보세요!",
+    images: [
+      {
+        // url: "/allabout-og-image.png",
+        width: 1200,
+        height: 630,
+        alt: "Og Image Alt",
+      },
+    ],
+  },
+};
