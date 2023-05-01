c6afc1fb66ce28e7d75199506d438b5fd7713867
picapipicca <haewon09106@gmail.com>
2023-05-01 16:47:16 +0900

    new:naver site verification

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,22 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="ko">
-      <Head />
+      <Head>
+      {/* <meta
+          name="google-site-verification"
+          content="HRgx6k0MKd5_lC_QsVHutowuuHGuZ-LWyylKQ5CUPqQ"
+        /> */}
+        <meta
+          name="naver-site-verification"
+          content="382dc40b39a7ceaecde59cedac5d7d0ff49f9bbb"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
