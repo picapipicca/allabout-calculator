8fc970baee0aea741d9f87a206478329f7133a45
picapipicca <haewon09106@gmail.com>
2023-05-09 02:45:03 +0900

    modif:database ) amount unique / api find create update arrange together as  upsert

--- pages/api/amount.ts
+++ pages/api/amount.ts
@@ -6,41 +6,24 @@ const handler = async (
   req: NextApiRequest,
   res: NextApiResponse<ResponseType>
 ) => {
-  // if (req.method === "GET") {
-  //   const amounts = await client.amount.findMany({
-  //     select: {
-  //       amount: true,
-  //       count: true,
-  //     },
-  //   });
-  //   res.json({ ok: true, amounts });
-  // } else if (req.method === "POST") {
   const {
     body: { income },
   } = req;
-  const isAmount = await client?.amount.findFirst({
+
+  await client?.amount.upsert({
     where: {
       amount: Number(income),
     },
-    select: {
-      id: true,
-      count: true,
+    create: {
+      amount: Number(income),
+    },
+    update: {
+      count: {
+        increment: 1,
+      },
     },
   });
 
-  if (!isAmount) {
-    await client?.amount.create({
-      data: {
-        amount: Number(income),
-        count: 1,
-      },
-    });
-  } else {
-    await client?.amount.update({
-      where: { id: isAmount?.id },
-      data: { count: isAmount?.count + 1 },
-    });
-  }
   res.json({
     ok: true,
   });
